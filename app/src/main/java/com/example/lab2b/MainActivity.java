3d14e89e4ea51585b45938513e3eff2944038ad3
bcochran210 <46578870+bcochran210@users.noreply.github.com>
2021-02-02 05:41:03 -0600

    Update - Added Array of possible choices - Added Button Functionality - Changed Size of the text

--- app/src/main/java/com/example/lab2b/MainActivity.java
+++ app/src/main/java/com/example/lab2b/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.lab2b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] helpItems;
+    private String helpItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helpItems = getResources().getStringArray(R.array.Help_List);
+        helpItem = "";
+    }
+
+    public void onClickGetHelpButton(View view) {
+        helpItem = getRandomIndex(helpItems);
+
+        TextView resultText = (TextView)findViewById(R.id.resultText);
+        resultText.setText(helpItem);
+    }
+
+    public static String getRandomIndex(String[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
     }
 }
